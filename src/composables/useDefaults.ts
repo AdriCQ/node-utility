20c5c845113a45ca6116bb83ef5e20a68006c4c7
AdriCQ <adriancapote95@gmail.com>
2024-03-07 10:21:41 -0500

    format code

--- src/composables/useDefaults.ts
+++ src/composables/useDefaults.ts
@@ -1,14 +1,14 @@
 export function useDefaults(params: Params | undefined) {
-
   return {
     handleImageError(event: Event) {
       const imageElement = event.target as HTMLImageElement;
-      imageElement.src = params?.image ?? 'https://coffective.com/wp-content/uploads/2018/06/default-featured-image.png.jpg';
+      imageElement.src =
+        params?.image ??
+        'https://coffective.com/wp-content/uploads/2018/06/default-featured-image.png.jpg';
     },
   };
 }
 
-
 interface Params {
-  image?: string
+  image?: string;
 }
--- src/composables/usePlatform.ts
+++ src/composables/usePlatform.ts
@@ -5,6 +5,6 @@
 export function usePlatform() {
   return {
     isMobile: () => window.matchMedia('(max-width: 767px)').matches,
-    isDesktop: () => window.matchMedia('(min-width: 767px)').matches
+    isDesktop: () => window.matchMedia('(min-width: 767px)').matches,
   };
 }
