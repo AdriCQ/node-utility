d5d4413d586267e0767836f9b5c16535e797997c
AdriCQ <adriancapote95@gmail.com>
2024-10-07 10:26:17 -0400

    feat: phone utils options

--- src/composables/usePhone.ts
+++ src/composables/usePhone.ts
@@ -2,27 +2,31 @@
  * usePhone
  */
 export function usePhone() {
-  function isCubanPhoneNumber(phoneNumber: string): boolean {
+  function isCubanPhoneNumber(
+    phoneNumber: string,
+    { withPrefix = false },
+  ): boolean {
     // Regular expression to match phone numbers starting with +53 followed by 8 digits
-    const regex = /^\+53\d{8}$/;
+    const regex = withPrefix ? /^\+53\d{8}$/ : /^\d{8}$/;
 
     // Test the phone number against the regex
     return regex.test(phoneNumber);
   }
 
-  function formatCubanPhoneNumber(phoneNumber: string) {
+  function formatCubanPhoneNumber(phoneNumber: string, { withPrefix = false }) {
     // Eliminar espacios en blanco
     let formattedNumber = phoneNumber.replace(/\s+/g, '');
 
     // Verificar si el número ya comienza con +53
-    if (!formattedNumber.startsWith('+53')) {
+    if (withPrefix && !formattedNumber.startsWith('+53')) {
       // Si no comienza con +53, agregarlo
       formattedNumber = '+53' + formattedNumber;
     }
 
     // Asegurarse de que solo queden los primeros 8 dígitos después de +53
-    formattedNumber =
-      formattedNumber.slice(0, 3) + formattedNumber.slice(3).slice(0, 8);
+    formattedNumber = withPrefix
+      ? formattedNumber.slice(0, 3) + formattedNumber.slice(3).slice(0, 8)
+      : formattedNumber.slice(0, 8);
 
     return formattedNumber;
   }
